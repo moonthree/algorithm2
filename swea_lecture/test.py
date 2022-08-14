8c30655558d33f188b25fd53e863e1e85918df62
moonthree <fpdhslr0725@gmail.com>
2022-08-14 13:27:14 +0900

    a

--- swea_lecture/test.py
+++ swea_lecture/test.py
@@ -1,5 +1,18 @@
-nums = []
-for _ in range(6):
-    nums.append(int(input().split()))
+a = int(input())
+b = input()
 
-print(nums)
+def chicken(num):
+    return num + 10
+
+def coke(str):
+    return str
+
+def kfc(num, str):
+    c = chicken(num)
+    d = coke(str)
+    print(f'{c}{d}')
+
+def main(a, b):
+    kfc(a, b)
+
+main(a, b)
