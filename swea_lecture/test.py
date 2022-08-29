7fe6d7755bc8031a6793f04caffc71defa631756
moonthree <fpdhslr0725@gmail.com>
2022-08-29 12:16:04 +0900

    a

--- swea_lecture/test.py
+++ swea_lecture/test.py
@@ -1,20 +1,53 @@
-n=int(input())
-result=[]
-# n보다 작은 수가 2번째에 와야함 
-# 2번째 수까지 li에 넣기
-for i in range(n):
-    li = []
-    li.append(n)
-    li.append(i)
-# 넣어진 리스트에서 뒤에서 2번째와 1번째의 차가 0보다작으면 그만 돌리기
-    while 1:
-        if li[-2]-li[-1]<0:
-            break
-        else:
-            li.append(li[-2]-li[-1])
-# 만들어진 리스트를 새리스트에 넣기
-    result.append(li)
-# 다 넣어진 result리스트를 각 for 문 돌아가면서 1~100까지 2번째 숫자 가 된 리스트의 길이를 카운트하여 구하기
-a=sorted(result, key=lambda x:-len(x))
-print(len(a[0]))
-print(*a[0])
+T = int(input())
+
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    directy1 = [-1, 1, 0, 0]
+    directx1 = [0, 0, -1, 1]
+    directy2 = [-1, 1, -1, 1]
+    directx2 = [1, 1, -1, -1]
+
+
+    def rowcol(y, x):
+        sum1 = arr[y][x]
+        for a in range(4):
+            for i in range(1, M):
+                ny1 = y + directy1[a]*(i)
+                nx1 = x + directx1[a]*(i)
+
+                if ny1<0 or nx1<0 or ny1>N-1 or nx1> N-1: continue
+
+                sum1 += arr[ny1][nx1]
+
+        return sum1
+
+    def diag(y, x):
+        sum2 = arr[y][x]
+        for b in range(4):
+            for j in range(1, M):
+                ny2 = y + directy2[b]*(j)
+                nx2 = x + directx2[b]*(j)
+
+                if ny2 < 0 or nx2 < 0 or ny2 > N - 1 or nx2 > N - 1: continue
+
+                sum2 += arr[ny2][nx2]
+
+        return sum2
+
+    MAX1 = 0
+    MAX2 = 0
+    for s in range(N):
+        for t in range(N):
+            if rowcol(s, t)> MAX1:
+                MAX1 = rowcol(s, t)
+            if diag(s, t)> MAX2:
+                MAX2 = diag(s, t)
+    print(MAX1)
+    print(MAX2)
+
+    if MAX1>MAX2:
+        print(f'#{tc} {MAX1}')
+    elif MAX2>=MAX1:
+        print(f'#{tc} {MAX2}')
