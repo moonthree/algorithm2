2e8e4553fcb29bc0efea53a4bf879422828aa8c6
moonthree <fpdhslr0725@gmail.com>
2022-09-05 18:02:00 +0900

    a

--- BaekJoon/simulation_ez/1331_knight_tour.py
+++ BaekJoon/simulation_ez/1331_knight_tour.py
@@ -1,24 +1,24 @@
-visited = [[0]*6 for _ in range(6)]
-
-dy = ['6', '5', '4', '3', '2', '1']
-dx = ['A', 'B', 'C', 'D', 'E', 'F']
-
-flag = 0
-
-for _ in range(36):
-    input_x, input_y = map(str, input())
-    visited[dy.index(input_y)][dx.index(input_x)] += 1
-
-    if visited[dy.index(input_y)][dx.index(input_x)] > 1:
-        flag = 1
-        break
-
-for y in range(6):
-    for x in range(6):
-        print(visited[y][x], end=' ')
-    print()
-
-if flag:
-    print('Invalid')
-else:
-    print('Valid')
+# visited = [[0]*6 for _ in range(6)]
+#
+# dy = ['6', '5', '4', '3', '2', '1']
+# dx = ['A', 'B', 'C', 'D', 'E', 'F']
+#
+# flag = 0
+#
+# for _ in range(36):
+#     input_x, input_y = map(str, input())
+#     visited[dy.index(input_y)][dx.index(input_x)] += 1
+#
+#     if visited[dy.index(input_y)][dx.index(input_x)] > 1:
+#         flag = 1
+#         break
+#
+# for y in range(6):
+#     for x in range(6):
+#         print(visited[y][x], end=' ')
+#     print()
+#
+# if flag:
+#     print('Invalid')
+# else:
+#     print('Valid')
